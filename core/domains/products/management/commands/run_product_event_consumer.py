9a9df56541dd97ad7ad66736dc7f879c06c41cf3
rahamali8876-hub <rahamali8876@gmail.com>
2026-01-05 10:17:48 +0000

    added logging to trake kafka folow

--- core/domains/products/management/commands/run_product_event_consumer.py
+++ core/domains/products/management/commands/run_product_event_consumer.py
@@ -1,8 +1,6 @@
 # filename : core/domains/products/management/commands/run_product_event_consumer.py
+import logging
 from django.core.management.base import BaseCommand
-# from core.domains.products.adapters.inbound.consumer.product_event_consumer import (
-#     run_product_search_consumer,
-# )
 from core.domains.products.adapters.inbound.consumer.product_event_consumer import (
     run_product_event_consumer,
 )
@@ -13,4 +11,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write("Product Event Consumer started")
+        logging.info(
+                "Run product event consumer Calling -> into this file :- ",
+                filename="core/domains/products/management/commands/run_product_event_consumer.py",
+            )
         run_product_event_consumer()
